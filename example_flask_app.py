d7e1026936d31ff51c1cc816cdce7e294900dabb
cheny2 <cheny2@cmc306-07.mathcs.carleton.edu>
2017-01-27 10:29:32 -0600

    Made modifications to example Flask server

--- example_flask_app.py
+++ example_flask_app.py
@@ -13,33 +13,37 @@ import sys
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return 'Hello, Citizen of CS257.'
-	
-@app.route('/fancier/')
+
+
+@app.route('/games/')
 def itDoesHTML():
-	htmlStr = '<html lang="en">' + \
-			  '<head>' + \
-			  '  <title>Cool page</title>' + \
-			  '</head>' + \
-			  '<body>' + \
-			  '  <h1>Welcome to CS 257</h1>' + \
-			  '  <p>Please refrain from feeding the squirrels.</p>' + \
-			  '</body>' + \
-			  '</html>'
-	return htmlStr
+    htmlStr = '<html lang="en">' + \
+              '<head>' + \
+              '  <title>Cool page</title>' + \
+              '</head>' + \
+              '<body>' + \
+              '  <h1>Welcome to CS 257</h1>' + \
+              '  <p>Please refrain from feeding the squirrels.</p>' + \
+              '</body>' + \
+              '</html>'
+    return htmlStr
+
 
 @app.route('/authors/<author>')
 def get_author(author):
     ''' What a dopey function! But it illustrates a Flask route with a parameter. '''
     if author == 'Twain':
-        author_dictionary = {'last_name':'Twain', 'first_name':'Mark'}
+        author_dictionary = {'last_name': 'Twain', 'first_name': 'Mark'}
     elif author == 'Shakespeare':
-		author_dictionary = {'last_name':'Shakespeare', 'first_name':'William'}
-	else:
-        author_dictionary = {'last_name':'McBozo', 'first_name':'Bozo'}
+        author_dictionary = {'last_name': 'Shakespeare', 'first_name': 'William'}
+    else:
+        author_dictionary = {'last_name': 'McBozo', 'first_name': 'Bozo'}
     return json.dumps(author_dictionary)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
