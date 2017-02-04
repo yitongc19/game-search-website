f90f5a37b9c4c108257818bc5e135d2c55a000f2
Chen Yitong <cheny2@eduroam-181-209.dyn.carleton.edu>
2017-02-04 18:10:36 -0500

    Editing the dataset

--- createtable.sql
+++ createtable.sql
@@ -1,8 +1,19 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time with time zone,
-  latitude real,
-  longitude real,
-  place text
+  Name text,
+  Platform text,
+  Year_of_Release real,
+  Genre text,
+  Publisher text,
+    NA_Sales real,
+    EU_Sales real,
+    JP_Sales real,
+    Other_Sales real,
+    Global_Sales real,
+    Critic_Score real,
+    Critic_Count real,
+    User_Score text,
+    User_Count real,
+    Developer text,
+    Rating text,
 );
