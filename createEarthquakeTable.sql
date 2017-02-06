72e53642fe67a83201de91ba5e098bffac885e20
zhonge <zhonge@cmc304-02.mathcs.carleton.edu>
2017-02-05 21:55:31 -0600

    testTableAdded

---
+++ createEarthquakeTable.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS earthquakes;
+CREATE TABLE earthquakes (
+  quakedate date,
+  quaketime time with time zone,
+  latitude real,
+  longitude real,
+  quakedepth real,
+  mag real,
+  place text
+);
