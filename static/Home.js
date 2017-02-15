716ec25beb2e59d9478cd7111138ba1ab1e897e0
Chen Yitong <cheny2@eduroam-181-209.dyn.carleton.edu>
2017-02-14 21:27:18 -0500

    Try using jinja hack

--- static/Home.js
+++ static/Home.js
@@ -1,5 +1,6 @@
 var getSearchResult = function () {
     var search_input = document.getElementById("search_keyword").value;
     var result_url = search_result_url.replace('NOTAKEY', search_input);
+    alert("searching!!!")
     window.location = result_url;
 }
